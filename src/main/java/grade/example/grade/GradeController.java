d4f8d9a5c539346a45e20669be1253a278606c5a
junior <juniormathiias@gmail.com>
2024-07-23 20:06:16 -0300

    feat: added a new values to read datas for map or each

--- src/main/java/grade/example/grade/GradeController.java
+++ src/main/java/grade/example/grade/GradeController.java
@@ -1,5 +1,8 @@
 package grade.example.grade;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class GradeController {
 
-    @GetMapping("/shows")
-    public String getGrades(Model model) {
-        Grade grade = new Grade("HARRY", "POTIONS", "C-");
-        model.addAttribute("grade", grade);
-        return "shows";
+    List<Grade> studentsGrades = Arrays.asList(
+        new Grade("HARRY", "POTIONS", "C-"),
+        new Grade("Kane", "TOP", "A-"),
+        new Grade("JASON", "MAMOA", "B+")
+    );
+
+    @GetMapping("/")
+    public String gradesForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "form";
     }
 
     @GetMapping("/grades")
     public String newProductForm(Model model) {
-        Grade grade = new Grade("HARRY", "POTIONS", "C-");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentsGrades);
         return "grades";
     }
 }
